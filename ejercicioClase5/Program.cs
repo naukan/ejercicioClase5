8a65de942db3893ef4417748de1a853e8531b590
Haitch <Haitch@HAITCH-PC>
2022-05-15 13:46:23 -0300

    Add project files.

---
+++ ejercicioClase5/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = "y";
+
+while (input == "y")
+{
+    Console.Clear();
+
+    Console.WriteLine("Escriba su nombre:");
+
+    string name = Console.ReadLine();
+
+    Console.WriteLine($"Hola {name}!");
+    Console.WriteLine("Desea continuar? Y/N");
+
+    input = Console.ReadLine();
+}
+
+ if (input.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente.");
+}
+else
+{
+    Console.WriteLine("Opcion no valida.");
+}
+
+
+
+
